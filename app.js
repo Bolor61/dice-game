f089894a48ca27891587429e30fbb379d650be3c
Bolor61 <63471365+Bolor61@users.noreply.github.com>
2020-05-15 18:12:39 -0500

    Toglogchiin eeljiig solidog bolgono

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 //  Тоглоочийн ээлжийг хадгалах хувьсагч, нэгдүгээр тоглогчийг 0, хоёрдугаар тоглогчийг 1 гэж тэмдэглэе.
-var activePlayer = 1;
+var activePlayer = 0;
 
 // Тоглочдын тоог цуглуулсан оноог хадгалах хувьсагч хэрэгтэй
 var scores = [0, 0];
@@ -12,6 +12,7 @@ var diceNumber = Math.floor(Math.random() * 6) + 1;
 // <div class="player-score" id="score-0">43</div>
 // window.document.querySelector("#score-0").textContent = dice;
 // document.querySelector("#score-1").innerHTML = "<em>Yes!</em>";
+
 // Программ эхлэхэд бэлтгэе
 document.getElementById("score-0").textContent = "0";
 document.getElementById("score-1").textContent = "0";
@@ -20,9 +21,45 @@ document.getElementById("current-1").textContent = "0";
 
 var diceDom = document.querySelector(".dice");
 diceDom.style.display = "none";
+// шоог шидэх event listener
 // Anonymous function нэг л удаа нэг л газар хэрэглэж байгаа учраас
 document.querySelector(".btn-roll").addEventListener("click", function () {
+  // 1-6 доторх санамсаргүй нэг тоог гаргаж авна
   var diceNumber = Math.floor(Math.random() * 6) + 1;
+
+  // Шооны зургийг вэб дээр гаргаж ирнэ.
   diceDom.style.display = "block";
+
+  // Буусан санамсаргүй тоонд шооны зургийг вэб дээр гаргаж ирнэ.
   diceDom.src = "dice-" + diceNumber + ".png";
+
+  //Буусан тоо нь 1 ээс ялгаатай бол идэвхитэй Тоглогчийн ээлжийн оноог нэмэгдүүлнэ.
+  if (diceNumber !== 1) {
+    // 1-ээс ялгаатай тоо буулаа. Буусан тоог тоглогчид нэмж өгнө
+    roundScore = roundScore + diceNumber;
+    document.getElementById("current-" + activePlayer).textContent = roundScore;
+  } else {
+    //  1 буусан тул тоглогчийн ээлжийг энэ хэсэгт сольж өгнө.
+
+    // энэ тоглогчийн ээлжиндээ цуглуулсан оноог 0 болгоно.
+    roundScore = 0;
+    document.getElementById("current-" + activePlayer).textContent = 0;
+
+    // Тоглогчийн ээлжийг нөгөө тоглогч руу шилжүүлнэ.
+    // хэрэв идэвхитэй тоглогч нь 0 байвал идэвхитэй тоглогчийг 1 болго.
+    //үгүй бол идэвхитэй тоглогчийг 0 болго.
+    activePlayer === 0 ? (activePlayer = 1) : (activePlayer = 0);
+    // Улаан цэгийг хайж олох
+    document.querySelector(" .player-0-panel").classList.toggle("active");
+    document.querySelector(" .player-1-panel").classList.toggle("active");
+
+    // Шоог түр алга болгох 1 гараад дараагын тоглогч эхлэхэд
+    diceDom.style.display = "none";
+
+    // if (activePlayer === 0) {
+    //   activePlayer = 1;
+    // } else {
+    //   activePlayer = 0;
+    // }
+  }
 });
